414b1c9101f381597a9e33b86506e2aeb7872156
Mark Blanford <markytorture@hotmail.com>
2018-06-18 17:15:36 +0100

    Added Data folder and ComicBookRepository to contain data and separate concerns from the Controller

--- ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/Controllers/ComicBooksController.cs
@@ -12,19 +12,6 @@ namespace ComicBookGallery.Controllers
 
         public ActionResult Detail()
         {
-            ComicBook comicBook = new ComicBook()
-            {
-                SeriesTitle = "The Amazing Spider-Man",
-                IssueNumber = 700,
-                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
-                Artists = new Artist[]{
-                    new Artist() { Role = "Script", Name = "Dan Slott" },
-                    new Artist() { Role = "Inks", Name = "Victor Olazaba" },
-                    new Artist() { Role = "Colors", Name = "Edgar Delgado" },
-                    new Artist() { Role = "Letters", Name = "Chris Eliopoulos" }
-                }
-            };
-
             return View(comicBook );
         } 
     }
