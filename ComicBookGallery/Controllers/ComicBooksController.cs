03a1b4f72d9ba6a43e84d21170e1b6ce118cb615
Mark Blanford <markytorture@hotmail.com>
2018-06-18 10:47:05 +0100

    Added first view for Comic Book Details

--- ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/Controllers/ComicBooksController.cs
@@ -11,12 +11,7 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
-            if(DateTime.Today.DayOfWeek == DayOfWeek.Monday)
-            {
-                return Redirect("/"); 
-            }
-
-            return Content("Welcome to the ComicBooks Controller!");
+            return View();
         }
     }
 }
