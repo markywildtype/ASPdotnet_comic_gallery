66a0450d2948249f4cb77e3d0e0049361f69de67
Mark Blanford <markytorture@hotmail.com>
2018-06-18 15:54:00 +0100

    Made detail view strongly typed

--- ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/Controllers/ComicBooksController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ComicBookGallery.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ComicBookGallery.Controllers
@@ -11,18 +12,20 @@ namespace ComicBookGallery.Controllers
 
         public ActionResult Detail()
         {
-            ViewBag.SeriesTitle = "The Amazing Spider-Man";
-            ViewBag.IssueNumber = 700;
-            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
-            ViewBag.Artists = new string[]{
-                "Script: Dan Slott",
-            "Pencils: Humberto Ramos",
-            "Inks: Victor Olazaba",
-            "Colors: Edgar Delgado",
-            "Letters: Chris Eliopoulos"
+            ComicBook comicBook = new ComicBook()
+            {
+                SeriesTitle = "The Amazing Spider-Man",
+                IssueNumber = 700,
+                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
+                Artists = new Artist[]{
+                    new Artist() { Role = "Script", Name = "Dan Slott" },
+                    new Artist() { Role = "Inks", Name = "Victor Olazaba" },
+                    new Artist() { Role = "Colors", Name = "Edgar Delgado" },
+                    new Artist() { Role = "Letters", Name = "Chris Eliopoulos" }
+                }
             };
 
-            return View();
-        }
+            return View(comicBook );
+        } 
     }
 }
--- ComicBookGallery/Models/ComicBook.cs
+++ ComicBookGallery/Models/ComicBook.cs
@@ -6,11 +6,25 @@ namespace ComicBookGallery.Models
     {
 
         public int Id { get; set; }
-        public string SeriesTitle { get; set;  }
+        public string SeriesTitle { get; set; }
         public int IssueNumber { get; set; }
-        public string DescriptionHtml  { get; set; }
+        public string DescriptionHtml { get; set; }
         public Artist[] Artists { get; set; }
         public bool Favourite { get; set; }
+
+        public string DisplayText
+        {
+            get {
+                return SeriesTitle + " #" + IssueNumber;
+            }
+        }
+
+        public string CoverImageFileName
+        { 
+            get {
+                return SeriesTitle.Replace(" ", "-").ToLower() + "-" + IssueNumber + ".jpg";
+            }
+        }
 
         public ComicBook()
         {
