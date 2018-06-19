8ceaff9e5b47b2e60780016979a472b651a9d85b
Mark Blanford <markytorture@hotmail.com>
2018-06-19 17:28:25 +0100

    Repository data added into the Controller

--- ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/Controllers/ComicBooksController.cs
@@ -4,15 +4,27 @@ using System.Linq;
 using System.Threading.Tasks;
 using ComicBookGallery.Models;
 using Microsoft.AspNetCore.Mvc;
+using ComicBookGallery.Data;
 
 namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
+        private ComicBookRepository _comicBookRepository = null;
 
-        public ActionResult Detail()
+        public ComicBooksController()
         {
-            return View(comicBook );
+            _comicBookRepository = new ComicBookRepository();
+        }
+
+        public ActionResult Detail(int? id)
+        {
+            if(id == null)
+            {
+                return new NotFoundResult();
+            }
+             var comicBook = _comicBookRepository.GetComicBook(id.Value);
+             return View(comicBook);
         } 
     }
 }
